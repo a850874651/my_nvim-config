2c1d0b1b7a5713bf10aa3f5fd66992d257dd7f75
a850874651 <850874651@qq.com>
2022-05-03 20:10:27 -0700

    first commit

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("colorscheme")
+-- 插件配置
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.telescope")
+require("plugin-config.dashboard")
+require("plugin-config.project")
+require("plugin-config.lualine")
+-- 内置LSP (新增)
+require("lsp.setup")
+require("lsp.cmp")
+require("plugin-config.nvim-treesitter")
---
+++ lua/plugins.lua
@@ -0,0 +1,83 @@
+local packer = require("packer")
+packer.startup({
+	function(use)
+		-- Packer 可以管理自己本身
+		use 'wbthomason/packer.nvim'
+		-- 你的插件列表...
+		--------------------- colorschemes --------------------
+		-- tokyonight
+		use("folke/tokyonight.nvim")
+		-- OceanicNext
+		use("mhartington/oceanic-next")
+		-- gruvbox
+		use({ "ellisonleao/gruvbox.nvim", requires = { "rktjmp/lush.nvim" } })
+		-- zephyr 暂时不推荐，详见上边解释
+		-- use("glepnir/zephyr-nvim")
+		-- nord
+		use("shaunsingh/nord.nvim")
+		-- onedark
+		use("ful1e5/onedark.nvim")
+		-- nightfox
+		use("EdenEast/nightfox.nvim")
+		-- nvim-tree
+		use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+		-- bufferline (新增)
+		use({ "akinsho/bufferline.nvim", requires = { "kyazdani42/nvim-web-devicons", "moll/vim-bbye" } })
+		-- lualine (新增)
+		use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons" } })
+		use("arkav/lualine-lsp-progress")
+		-- telescope （新增）
+		use { 'nvim-telescope/telescope.nvim', requires = { "nvim-lua/plenary.nvim" } }
+		use "LinArcx/telescope-env.nvim"
+		use("glepnir/dashboard-nvim")
+		-- project
+		use("ahmedkhalf/project.nvim")
+		-- treesitter （新增）
+		use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+		--------------------- LSP --------------------
+		use({ "williamboman/nvim-lsp-installer", commit = "36b44679f7cc73968dbb3b09246798a19f7c14e0" })
+		-- Lspconfig
+		use({ "neovim/nvim-lspconfig" })
+		-- 补全引擎
+		use("hrsh7th/nvim-cmp")
+		-- snippet 引擎
+		use("hrsh7th/vim-vsnip")
+		-- 补全源
+		use("hrsh7th/cmp-vsnip")
+		use("hrsh7th/cmp-nvim-lsp") -- { name = nvim_lsp }
+		use("hrsh7th/cmp-buffer") -- { name = 'buffer' },
+		use("hrsh7th/cmp-path") -- { name = 'path' }
+		use("hrsh7th/cmp-cmdline") -- { name = 'cmdline' }
+
+		-- 常见编程语言代码段
+		use("rafamadriz/friendly-snippets")
+
+		-------------------------------------------------------
+	end,
+	config = {
+		-- 并发数限制
+		max_jobs = 16,
+		-- 自定义源
+		git = {
+			-- default_url_format = "https://hub.fastgit.xyz/%s",
+			-- default_url_format = "https://mirror.ghproxy.com/https://github.com/%s",
+			-- default_url_format = "https://gitcode.net/mirrors/%s",
+			-- default_url_format = "https://gitclone.com/github.com/%s",
+		},
+		display = {
+			open_fn = function()
+				return require("packer.util").float({ border = "single" })
+			end,
+		},
+	},
+})
+-- 每次保存 plugins.lua 自动安装插件
+pcall(
+	vim.cmd,
+	[[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)
